c12fd7f82b2ff94cd0117e66fbeeb52c4d8e2b3e
juansantacruz <juann.santacruz@gmail.com>
2017-09-08 18:10:02 -0300

    edit/delet category

---
+++ Presentation/ASF.UI.WbSite/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASF.UI.Process;
+using ASF.Entities;
+
+namespace ASF.UI.WbSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var cp = new CategoryProcess();
+            return View(cp.SelectList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            var cp = new CategoryProcess();
+            cp.insertCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var cp = new CategoryProcess();
+
+            return View(cp.findCategory(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            var cp = new CategoryProcess();
+            cp.editCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var cp = new CategoryProcess();
+
+            return View(cp.findCategory(id));
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Category category)
+        {
+            var cp = new CategoryProcess();
+            cp.deleteCategory(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
